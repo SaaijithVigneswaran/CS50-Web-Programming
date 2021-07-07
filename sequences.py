a91a54ae68b5ad149f65ff7c6fb7b9bbd3b63954
SaaijithVigneswaran <saaijith27@gmail.com>
2021-07-06 21:07:28 -0400

    lists

---
+++ sequences.py
@@ -0,0 +1,9 @@
+COORDINATES = (20.0,10.0) ##TUPPLE (list tupple sequemce of immutable pairs ,set, dict= collection of key value pairs)
+print(COORDINATES)
+
+names = ["Tom", "Jithzz", "Bob", "Lion"] ## lists mutable
+print(names)
+names.append("Draco")
+
+names.sort()
+print(names)
